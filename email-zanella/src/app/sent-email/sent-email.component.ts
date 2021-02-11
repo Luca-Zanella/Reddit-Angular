2b2ba196595876bbfafbc5d632f4845f72e5cea1
Zanella Luca <zanella.luca@iisgalvanimi.edu.it>
2021-02-11 11:41:00 +0100

    fix all point

--- email-zanella/src/app/sent-email/sent-email.component.ts
+++ email-zanella/src/app/sent-email/sent-email.component.ts
@@ -1,23 +1,53 @@
 import { Component, HostBinding, Input, OnInit } from '@angular/core';
-import {Email} from '../models/email.model'
+import { Email } from '../models/email.model';
 
 @Component({
   selector: 'app-sent-email',
   templateUrl: './sent-email.component.html',
-  styleUrls: ['./sent-email.component.css']
+  styleUrls: ['./sent-email.component.css'],
 })
 export class SentEmailComponent implements OnInit {
   @Input() email: Email;
   @HostBinding('attr.class') cssClass = 'card';
-  isShow: boolean = true
+  @HostBinding('style.background') private background;
 
-  constructor() { }
+  isShow: boolean = true;
+  //bgColor : string = '#ffffff';
+  agg: boolean = true;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  espandi() {
+    this.isShow = !this.isShow;
+  }
+  
+  /*
+  speciale(){
+
+    if(this.agg == true){
+      this.background = 'yellow';
+      this.agg = false
+    }else{
+      this.background = 'white'
+      this.agg = true;
+    }
+    
+    
+  }
+  */
+
+
+  speciale(): string {
+    if(! this.email.speciale()){
+      return this.background = 'white'
+    }else{
+      return this.background = 'yellow'
+    }
+
+    //return (this.background = 'yellow');
   }
 
-  espandi(){
-    this.isShow = ! this.isShow;
-  }
-
+  //return this.background = 'yellow'
 }
